f6eeeee59e4815d67b51142eee6fe46622a12566
Marco-Willian <marcowcandido@gmail.com>
2023-12-03 13:12:19 -0300

    Refactor entire api to use ApplicationUser

--- JwtAuth/Core/Dtos/RegisterDto.cs
+++ JwtAuth/Core/Dtos/RegisterDto.cs
@@ -4,6 +4,12 @@ namespace JwtAuth.Core.Dtos
 {
     public class RegisterDto
     {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
         [Required(ErrorMessage = "User Name is required")]
         public string UserName { get; set; }
 
